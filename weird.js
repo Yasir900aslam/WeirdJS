bc503c212f022a1998606e59e7060f9ae3762d9d
yasir <yasir900aslam@gmail.com>
2021-03-13 02:00:12 +0500

    Weird 1

--- weird.js
+++ weird.js
@@ -1,3 +1,21 @@
 // I don't have any explanation for this shit ass cold fucking freezing mind fuck code.
 var f = (function f(){ return "1"; }, function g(){ return 2; })();
 console.log(typeof f)
+
+
+// whoever wrote this the first time, you need to goto therapy.
+// I mean why do you need this?
+// Whyyy?
+var foo = {
+    bar: function(){ return this.baz; },
+    baz: 1
+};
+console.log(typeof (f = foo.bar)());
+
+
+
+var x = 1;
+if (function f(){}) {
+    x += typeof f;
+}
+x;
